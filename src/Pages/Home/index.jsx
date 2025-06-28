1bf49400140c8af5c4e53bc8ac68394cc7e0661c
flaviodrummond <flaviosax20@gmail.com>
2025-06-27 22:34:07 -0300

    Instação do axios, criação da pasta services e criado a estrutura axios

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,26 @@
+import {useRef} from "react";
+import api from "../../Services/api";
 import { Titulo, Container, ImagemCentral, Form, CaixaDeInput, Input, InputLabel, Button } from "./style";
 
 import Usuarios from '../../assets/users.png'
 
 function Home() {
+
+  const PegarNome = useRef()
+  const PegarCpf = useRef()
+  const PegarEmail = useRef()
+
+  async function RegistrarNovoUsuario() {
+    const data =  await api.post('/usuarios', {
+      name: PegarNome.current.value,
+      cpf: PegarCpf.current.value,
+      email: PegarEmail.current.value
+    })
+
+    console.log(data)
+  }
+
+  
 
 
   return (
@@ -23,14 +41,14 @@ function Home() {
               <InputLabel>
                 Nome <span>*</span>
               </InputLabel>
-              <Input type="text" placeholder="Nome do usuário" />
+              <Input type="text" placeholder="Nome do usuário" ref={PegarNome} />
             </div>
 
             <div>
               <InputLabel>
                 CPF <span>*</span>
               </InputLabel>
-              <Input type="number" placeholder="CPF do usuário" />
+              <Input type="number" placeholder="CPF do usuário" ref={PegarCpf} />
             </div>
 
         </CaixaDeInput>
@@ -39,10 +57,10 @@ function Home() {
           <InputLabel>
             E-mail <span>*</span>
           </InputLabel>
-          <Input type="email" placeholder="E-mail do usuário" />
+          <Input type="email" placeholder="E-mail do usuário" ref={PegarEmail} />
         </div>
 
-        <Button>Cadastrar Cliente</Button>
+        <Button type="button" onClick={RegistrarNovoUsuario}>Cadastrar Cliente</Button>
       </Form>
 
     </Container>
