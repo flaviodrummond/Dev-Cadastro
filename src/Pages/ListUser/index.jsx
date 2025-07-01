e9f853b44408886387701b8dbbc9ab01f702cf42
flaviodrummond <flaviosax20@gmail.com>
2025-07-01 02:08:39 -0300

    Aprendendo sobre useEffect e adicionando a nossa aplicação

--- src/Pages/ListUser/index.jsx
+++ src/Pages/ListUser/index.jsx
@@ -1,7 +1,19 @@
+import api from "../../Services/api"
+import { useEffect } from "react"
 import Button from "../../Components/Button"
 import ImagemCentral from "../../Components/ImagemCentral"
 
 function ListUser() {
+
+    useEffect(() => {
+        async function getUsuario() {
+          const ApiUsuario = await api.get('./usuario')
+            console.log(ApiUsuario)
+        }
+        getUsuario()
+    }, [])
+    
+
     return (
         <div>
             <ImagemCentral/>
